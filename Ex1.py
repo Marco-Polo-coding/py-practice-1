06dd342c74db59adcdc295bd61522ea370c205f0
Marco-Polo-coding <dawtardeiestiernogalvan@gmail.com>
2023-10-20 19:17:45 +0200

    Ejercicio1

--- Ex1.py
+++ Ex1.py
@@ -1,6 +1,7 @@
 # Main concepts of strings, numbers, booleans, and lists in Python:
 # check lessons 07, 08 from py-foundations
 
+from datetime import date
 
 story = """Hace dos décadas, Javier, un joven programador apasionado, estaba decidido a crear el software definitivo. Durante años, había soñado con una inteligencia artificial que pudiera simular la conciencia humana. Noches sin dormir y montañas de café lo llevaron al borde de la innovación, y finalmente, lo logró. Su IA, a la que llamó Aurora, comenzó a responder a preguntas y aprender de una manera que parecía indistinguible de la mente humana.
 
@@ -23,13 +24,15 @@ Exercise 2:** Calculate the number of YEARS until 2045.
 Use the variable `year` to store the current year.
 '''
 year = 2023 # get current year
-years_left = 0 # write your code here
+years_left = 2045-year # write your code here
 print("Number of years left:", years_left)
 
 '''
 Exercise 3:** Calculate the number of DAYS until 2045.
 '''
-days_left = 0 # write your code here
+fecha_inicio = date(2023, 10, 20)
+fecha_fin = date (2045, 10, 20)
+days_left = fecha_fin - fecha_inicio # write your code here
 print("Number of days left:", days_left)
 
 # 3. Booleans
@@ -37,7 +40,8 @@ print("Number of days left:", days_left)
 '''
 Exercise 4:** Check if the word "IA" is includes in the story.
 '''
-ia_in_story = False # write your code here
+contains_ia = "ia" in story
+ia_in_story = contains_ia # write your code here
 if (ia_in_story):
   print("The word 'IA' is in the story")
 else:
@@ -48,35 +52,38 @@ else:
 '''
 Exercise 5:** Create a list of the words in the story and print the first 10 words.
 '''
-words = [] # write your code here
-print("First 10 words in the story:", words)
+words = story.split() # write your code here
+ten_first_words = words[:10]
+print("First 10 words in the story:", ten_first_words)
 
 '''
 Exercise 6:** Create a list of the words in the story and print the last 10 words.
 '''
-print("Last 10 words in the story:", words)
+ten_last_words = words[-10:]
+print("Last 10 words in the story:", ten_last_words)
 
 '''
 Exercise 7:** Create a list of the words in the story and print the words in the 10th to 20th position.
 '''
-print("Words in the 10th to 20th position:", words)
+ten_to_tweny_words = words[10:20]
+print("Words in the 10th to 20th position:", ten_to_tweny_words)
 
 '''
 Exercise 8:** Count the number of times the word "Javier" appears in the story.
 '''
-javier_count = 0 # write your code here
+javier_count = story.count("Javier") # write your code here
 print("Number of times the word 'Javier' appears in the story:", javier_count)
 
 '''
 Exercise 9:** Replace the word "Javier" with "Pepito" in the story.
 '''
-story2 = "" # write your code here
+story2 = story.replace("Javier", "Pepito") # write your code here
 print("Story with 'Javier' replaced with 'Pepito':", story2)
 
 '''
 Exercise 10:** Reverse the order of the words in the story.
 '''
-story3 = "" # write your code here
+story3 = story[::-1] # write your code here
 print("Story with words reversed:", story3)
 
 '''
